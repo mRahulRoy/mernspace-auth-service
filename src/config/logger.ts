67d6368cf8d76d0c9e2bd68e9e1c84e0624cd757
rahul roy <rahulroy177602@gmail.com>
2023-09-22 14:23:32 +0530

    logger setup

---
+++ src/config/logger.ts
@@ -0,0 +1,26 @@
+import winston from 'winston';
+import { Config } from '.';
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: {
+        serviceName: 'auth-service',
+    },
+    transports: [
+        new winston.transports.File({
+            dirname: 'logs',
+            filename: 'combined.log',
+            level: 'info',
+            silent: Config.NODE_ENV === 'test',
+        }),
+        new winston.transports.File({
+            dirname: 'logs',
+            filename: 'error.log',
+            level: 'error',
+            silent: Config.NODE_ENV === 'test',
+        }),
+    ],
+});
+
+export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
 import app from './app';
 import { Config } from './config/index';
+import logger from './config/logger';
 
 const startServer = () => {
     const { PORT } = Config;
     try {
         // eslint-disable-next-line no-console
+        // throw new Error("somet thing went wrong")
         app.listen(PORT, () => {
-            console.log('listening on port ', Config.PORT);
+            logger.info(`Listening on port ${PORT}`);
         });
     } catch (error) {
         // eslint-disable-next-line no-console
-        console.log(error);
-        process.exit(1);
+        if (error instanceof Error) {
+            logger.error(error.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 };
 
